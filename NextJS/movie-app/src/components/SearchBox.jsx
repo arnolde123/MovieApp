f6f1f192cc1bcd8a0970836c09f679c300c2c9e8
Arnold Elamthuruthil <aelamthuruthil@gmail.com>
2025-05-15 11:39:50 -0400

    added working search box

--- NextJS/movie-app/src/components/SearchBox.jsx
+++ NextJS/movie-app/src/components/SearchBox.jsx
@@ -1,21 +1,37 @@
 "use client"
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Button } from './ui/button'
-
 
 export default function SearchBox() {
   const [search, setSearch] = useState('')
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return
+    router.push(`/search/${encodeURIComponent(search)}`)
+  }
   return (
-    <div>
-      <Input type="text" placeholder="Search keywords..." className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1" value={search} onChange={(e) => setSearch(e.target_value)}/>
+    <form 
+      className='flex justify-between px-5 max-w-6xl mx-auto' 
+      onSubmit={handleSubmit}
+    >
+      <Input 
+        type="text"
+        placeholder="Search keywords..."
+        className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <Button
-        className="text-green-500 disabled:text-gray-500"
+        variant="ghost"
+        className="text-green-500 disabled:text-gray-500 justify-center mt-2"
         disabled={search === ''}
       >
         Search
       </Button>
-    </div>
+    </form>
   )
 }
